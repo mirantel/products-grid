6d976c50f09595e64fb6710a3f117dbab2136827
mirantel <magi.mirantel@gmail.com>
2013-11-13 02:29:43 +0200

    added new grids

--- js/common.js
+++ js/common.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
 
-$('.class-num').click(function(){
-	$( ".products" ).addClass('products_'+ $(this).val())
+$('.radio_col').on('click', function(){
+	$( ".products" ).removeClass('products_col_1 products_col_2 products_col_3')
+	.addClass('products_'+ this.value);
+});
+
+$('.radio_info').on('click', function(){
+	$( ".products" ).removeClass('products_info_left products_info_center products_info_right')
+	.addClass('products_'+ this.value);
+});
+
+$('.radio_info_vertical').on('click', function(){
+	$( ".products" ).removeClass('products_info_above products_info_below')
+	.addClass('products_'+ this.value);
 });
 
 });
