6866166c98913f2eec06e425162fc603d06c70a6
mirantel <magi.mirantel@gmail.com>
2013-11-28 17:00:16 +0200

    Updated grid

--- js/common.js
+++ js/common.js
@@ -1,22 +1,16 @@
 $(document).ready(function() {
 
-$('.radio_col').on('click', function(){
-	$( ".products" ).removeClass('products_col_1 products_col_2 products_col_3')
-	.addClass('products_'+ this.value);
-});
-
 $('.radio_info').on('click', function(){
 	$( ".products" ).removeClass('products_info_left products_info_center products_info_right')
 	.addClass('products_'+ this.value);
 });
 
-$('.radio_info_vertical').on('click', function(){
-	$( ".products" ).removeClass('products_info_above products_info_below')
-	.addClass('products_'+ this.value);
+$('.fix-height').change( function(){
+	$( ".products" ).toggleClass("products_fix_height", this.checked);
 });
 
 $('.radio_grid_type').on('click', function(){
-	$( ".products" ).removeClass('products_big_little products_2_plus_1 products_steps products_img_cell products_big_little_vert')
+	$( ".products" ).removeClass('products_grid_1 products_grid_2 products_grid_3 products_grid_4 products_grid_5 products_grid_6 products_grid_7 products_grid_8')
 	.addClass('products_'+ this.value);
 });
 });
